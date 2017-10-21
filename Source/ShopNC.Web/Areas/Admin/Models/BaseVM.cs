a628c1d503f87aaca2fa558e9ff942d994901fca
chi8708 <983143462@qq.com>
2017-10-21 11:28:20 +0800

    basevm 添加

---
+++ Source/ShopNC.Web/Areas/Admin/Models/BaseVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopNC.Web.Areas.Admin.Models
+{
+    public class BaseVM
+    {
+        /// <summary>
+        /// 逻辑上的删除，非物理删除
+        /// </summary>
+        public bool IsDeleted { get; set; }
+    }
+}
